2997c9ca1b25a652d9ff5a9e304b0a4d76d2bd3a
teojiesern <teojiesern42@gmail.com>
2023-06-20 00:46:21 +0800

    'commit'

--- src/AudioRecorder.jsx
+++ src/AudioRecorder.jsx
@@ -1,92 +1,43 @@
 import { useState, useRef } from "react";
 const mimeType = "audio/wav";
 const AudioRecorder = () => {
-    // const [permission, setPermission] = useState(false);
-    // const mediaRecorder = useRef(null);
-    // const [recordingStatus, setRecordingStatus] = useState("inactive");
-    // const [stream, setStream] = useState(null);
-    // const [audioChunks, setAudioChunks] = useState([]);
-    // const [audio, setAudio] = useState(null);
-
-    // const getMicrophonePermission = async () => {
-    //     if ("MediaRecorder" in window) {
-    //         try {
-    //             const streamData = await navigator.mediaDevices.getUserMedia({
-    //                 audio: true,
-    //                 video: false,
-    //             });
-    //             setPermission(true);
-    //             setStream(streamData);
-    //         } catch (err) {
-    //             alert(err.message);
-    //         }
-    //     } else {
-    //         alert("The MediaRecorder API is not supported in your browser.");
-    //     }
-    // };
-
-    // const startRecording = async () => {
-    //     setRecordingStatus("recording");
-    //     //create new Media recorder instance using the stream
-    //     const media = new MediaRecorder(stream, { type: mimeType });
-    //     //set the MediaRecorder instance to the mediaRecorder ref
-    //     mediaRecorder.current = media;
-    //     //invokes the start method to start the recording process
-    //     mediaRecorder.current.start();
-    //     let localAudioChunks = [];
-    //     mediaRecorder.current.ondataavailable = (event) => {
-    //         if (typeof event.data === "undefined") return;
-    //         if (event.data.size === 0) return;
-    //         localAudioChunks.push(event.data);
-    //     };
-    //     setAudioChunks(localAudioChunks);
-    // };
-
-    // const stopRecording = () => {
-    //     setRecordingStatus("inactive");
-    //     //stops the recording instance
-    //     mediaRecorder.current.stop();
-    //     mediaRecorder.current.onstop = () => {
-    //         //creates a blob file from the audiochunks data
-    //         const audioBlob = new Blob(audioChunks, { type: mimeType });
-    //         sendAudioDataToBackend(audioBlob);
-    //         //creates a playable URL from the blob file.
-    //         const audioUrl = URL.createObjectURL(audioBlob);
-    //         setAudio(audioUrl);
-    //         setAudioChunks([]);
-    //     };
-    // };
-
     const [permission, setPermission] = useState(false);
+    const mediaRecorder = useRef(null);
     const [recordingStatus, setRecordingStatus] = useState("inactive");
+    const [stream, setStream] = useState(null);
     const [audioChunks, setAudioChunks] = useState([]);
     const [audio, setAudio] = useState(null);
 
     const getMicrophonePermission = async () => {
-        try {
-            const streamData = await navigator.mediaDevices.getUserMedia({
-                audio: true,
-                video: false,
-            });
-            setPermission(true);
-        } catch (err) {
-            alert(err.message);
+        if ("MediaRecorder" in window) {
+            try {
+                const streamData = await navigator.mediaDevices.getUserMedia({
+                    audio: true,
+                    video: false,
+                });
+                setPermission(true);
+                setStream(streamData);
+            } catch (err) {
+                alert(err.message);
+            }
+        } else {
+            alert("The MediaRecorder API is not supported in your browser.");
         }
     };
 
     const startRecording = async () => {
         setRecordingStatus("recording");
-        const context = new AudioContext();
-        const source = context.createMediaStreamSource(streamData);
-        const processor = context.createScriptProcessor(1024, 1, 1);
-
-        source.connect(processor);
-        processor.connect(context.destination);
-
+        //create new Media recorder instance using the stream
+        const media = new MediaRecorder(stream, { type: mimeType });
+        //set the MediaRecorder instance to the mediaRecorder ref
+        mediaRecorder.current = media;
+        //invokes the start method to start the recording process
+        mediaRecorder.current.start();
         let localAudioChunks = [];
-        processor.onaudioprocess = (e) => {
-            const audioData = e.inputBuffer.getChannelData(0);
-            localAudioChunks.push(audioData);
+        mediaRecorder.current.ondataavailable = (event) => {
+            if (typeof event.data === "undefined") return;
+            if (event.data.size === 0) return;
+            localAudioChunks.push(event.data);
         };
         setAudioChunks(localAudioChunks);
     };
@@ -94,15 +45,64 @@ const AudioRecorder = () => {
     const stopRecording = () => {
         setRecordingStatus("inactive");
         //stops the recording instance
-        processor.disconnect();
-        //creates a blob file from the audiochunks data
-        const audioBlob = new Blob(audioChunks, { type: mimeType });
-        sendAudioDataToBackend(audioBlob);
-        //creates a playable URL from the blob file.
-        const audioUrl = URL.createObjectURL(audioBlob);
-        setAudio(audioUrl);
-        setAudioChunks([]);
+        mediaRecorder.current.stop();
+        mediaRecorder.current.onstop = () => {
+            //creates a blob file from the audiochunks data
+            const audioBlob = new Blob(audioChunks, { type: mimeType });
+            sendAudioDataToBackend(audioBlob);
+            //creates a playable URL from the blob file.
+            const audioUrl = URL.createObjectURL(audioBlob);
+            setAudio(audioUrl);
+            setAudioChunks([]);
+        };
     };
+
+    // const [permission, setPermission] = useState(false);
+    // const [recordingStatus, setRecordingStatus] = useState("inactive");
+    // const [audioChunks, setAudioChunks] = useState([]);
+    // const [audio, setAudio] = useState(null);
+
+    // const getMicrophonePermission = async () => {
+    //     try {
+    //         const streamData = await navigator.mediaDevices.getUserMedia({
+    //             audio: true,
+    //             video: false,
+    //         });
+    //         setPermission(true);
+    //     } catch (err) {
+    //         alert(err.message);
+    //     }
+    // };
+
+    // const startRecording = async () => {
+    //     setRecordingStatus("recording");
+    //     const context = new AudioContext();
+    //     const source = context.createMediaStreamSource(streamData);
+    //     const processor = context.createScriptProcessor(1024, 1, 1);
+
+    //     source.connect(processor);
+    //     processor.connect(context.destination);
+
+    //     let localAudioChunks = [];
+    //     processor.onaudioprocess = (e) => {
+    //         const audioData = e.inputBuffer.getChannelData(0);
+    //         localAudioChunks.push(audioData);
+    //     };
+    //     setAudioChunks(localAudioChunks);
+    // };
+
+    // const stopRecording = () => {
+    //     setRecordingStatus("inactive");
+    //     //stops the recording instance
+    //     processor.disconnect();
+    //     //creates a blob file from the audiochunks data
+    //     const audioBlob = new Blob(audioChunks, { type: mimeType });
+    //     sendAudioDataToBackend(audioBlob);
+    //     //creates a playable URL from the blob file.
+    //     const audioUrl = URL.createObjectURL(audioBlob);
+    //     setAudio(audioUrl);
+    //     setAudioChunks([]);
+    // };
 
     const sendAudioDataToBackend = async (audioBlob) => {
         try {
